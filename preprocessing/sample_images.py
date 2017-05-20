4fae47b1de0b13149c1dd50d8ba00fff8b0158d8
Eric <ericchang00@gmail.com>
2017-05-20 00:31:10 -0500

    train and test dir creation

--- preprocessing/sample_images.py
+++ preprocessing/sample_images.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import random
 from tqdm import tqdm
 
 
@@ -8,8 +9,8 @@ def sample_images(n, base_path, new_path=None, dir_keyword=None):
     create a new directory with a subsample of the images in the current directory
     
     ex:
-        base_path = '~/images'
-        new_path = '~/images'
+        base_path = '~/project/images'
+        new_path = '~/project/images-sample-1000'
     """
     if not new_path:
         new_path = base_path + "-sample-" + str(n)
@@ -30,6 +31,34 @@ def sample_images(n, base_path, new_path=None, dir_keyword=None):
                             os.path.join(new_path, dir_, file))
 
 
+def create_train_test_dirs(path, train_test_split):
+    """
+    create train and test directories given a splitting proportion as a tuple
+    e.g. train_test_split = (.8, .2)
+    """
+    assert train_test_split[0] + train_test_split[1] == 1, "split must sum to 1"
+
+    train_dir = os.path.join(path, 'train')
+    test_dir = os.path.join(path, 'test')
+    os.makedirs(train_dir)
+    os.makedirs(test_dir)
+
+    dirs = []
+    for d in next(os.walk(path))[1]:
+        if d not in ['train', 'test']:
+            dirs.append(d)
+
+    for dir_ in dirs:
+        os.makedirs(os.path.join(train_dir, dir_), exist_ok=True)
+        os.makedirs(os.path.join(test_dir, dir_), exist_ok=True)
+        files = os.listdir(os.path.join(path, dir_))
+        train_files = set(random.sample(files, round(len(files) * train_test_split[0])))
+        test_files = [f for f in files if f not in train_files]
+        for f in train_files:
+            shutil.copyfile(os.path.join(path, dir_, f), os.path.join(train_dir, dir_, f))
+        for f in test_files:
+            shutil.copyfile(os.path.join(path, dir_, f), os.path.join(test_dir, dir_, f))
+
+
 if __name__ == "__main__":
-    base_path = "/home/lab.analytics.northwestern.edu/echang/TEAMSHAREDFOLDER/flickr-64"
-    sample_images(3000, base_path, dir_keyword="processed")
+    pass
